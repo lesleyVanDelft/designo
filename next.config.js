43191883ae5738b37674fe216f7d2a9b0bf4e2e6
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-07-27 20:08:04 +0200

    cleanup

--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@
 module.exports = {
 	reactStrictMode: true,
 	swcMinify: true,
-	images: {
-		imageSizes: [16, 32, 48, 64, , 90, 96, 128, 256, 384, 500, 800, 1024],
-	},
 	experimental: {
 		images: {
 			allowFutureImage: true,
