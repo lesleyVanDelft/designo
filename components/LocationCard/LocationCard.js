798a80742cdc77a4b3d0ba5f6a816523f3f90a13
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-08-22 18:36:44 +0200

    add locations tablet styling

--- components/LocationCard/LocationCard.js
+++ components/LocationCard/LocationCard.js
@@ -7,33 +7,24 @@ const LocationCard = ({ data }) => {
 	// console.log(data);
 	return (
 		<div className={styles.LocationCard}>
-			<figure className={styles.LocationCard__image}>
+			<div className={styles.LocationCard__map}>
 				<GoogleMaps location={data.location} />
-			</figure>
-			{/* <figure className={styles.LocationCard__image}>
-				<Image
-					src={data.mapImageLarge}
-					alt={'image of a map'}
-					// layout="raw"
-					layout="responsive"
-					width={375}
-					height={320}
-				/>
-			</figure> */}
+			</div>
 
 			<div className={styles.LocationCard__info}>
 				<h2>{data.name}</h2>
-				<ul>
-					<li>{data.address.name}</li>
-					<li>{data.address.street}</li>
-					<li>{data.address.location}</li>
-				</ul>
-
-				<ul>
-					<li>Contact</li>
-					<li>{data.contact.phone}</li>
-					<li>{data.contact.email}</li>
-				</ul>
+				<div className={styles.LocationCard__info_lists}>
+					<ul>
+						<li>{data.address.name}</li>
+						<li>{data.address.street}</li>
+						<li>{data.address.location}</li>
+					</ul>
+					<ul>
+						<li>Contact</li>
+						<li>{data.contact.phone}</li>
+						<li>{data.contact.email}</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
